93cb20cafc0de92dc85bb177791df4ca7bde77eb
Isaac Frank <codingwithisaac@gmail.com>
2021-11-25 14:21:05 +0100

    vlh

---
+++ src/App.js
@@ -0,0 +1,277 @@
+import './App.css';
+import {useState, useEffect} from 'react'
+import {ADDRESS, ABI, NETWORK_ID} from './constants';
+
+function App() {
+  
+  const $ = window.jQuery;
+
+  const [error, setError] = useState("");
+  const [errorA, setErrorA] = useState("");
+  const [errorAB, setErrorAB] = useState(false);
+  const [softcap, setSoftcap] = useState(0);
+  const [hardcap, setHardcap] = useState(0);
+  const [raised, setRaised] = useState(0);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
+  const [mine, setMine] = useState(0);
+  const [isWhitelisted, setIsWhiteListed] = useState(false);
+  const [onlyWhitelisted, setOnlyWhitelisted] = useState(false);
+  const [account, setAccount] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [contract, setContract] = useState("");
+  const [web3, setWeb3] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [started, setStarted] = useState(false);
+  const [completed, setCompleted] = useState(false);
+  const [id, setId] = useState('');
+  const [block, setBlock] = useState('');
+
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setBlock(b=>!b);
+    }, 3000);
+    return () => {clearInterval(i)};
+  },[]);
+
+  useEffect(() => {
+    const  setup = async (_contract) => {
+      let hasStarted = await _contract.methods.openingTime().call();
+      let hasClosed = await _contract.methods.hasClosed().call()
+      let finalized = await _contract.methods
+        .isFinalized()
+        .call()
+
+      if (hasClosed || finalized) {
+        setCompleted(true);
+      }
+      if (hasStarted <= Date.now() / 1000) {
+        setStarted(true);
+      }
+    }
+    if(web3){
+      const presale = new web3.eth.Contract(ABI, ADDRESS);
+      presale.methods.goal().call().then(setSoftcap);
+      presale.methods.cap().call().then(setHardcap);
+      presale.methods.weiRaised().call().then(setRaised);
+      presale.methods.minContribution().call().then(setMin);
+      presale.methods.maxContribution().call().then(setMax);
+      presale.methods.onlyWhitelisted().call().then(setOnlyWhitelisted);
+      setup(presale);
+      setContract(presale);
+
+      if(id === ''){
+        web3.eth.net.getId().then(_id => setId(_id));
+      }
+      web3.currentProvider.on("accountsChanged", function (accounts) {
+        setAccount(accounts[0]);
+      });
+
+      web3.currentProvider.on("networkChanged", function (networkId) {
+        web3.eth.net.getId().then((id) => setId(id));
+      });
+    }
+  },[web3])
+
+  useEffect(()=>{
+    if(web3){
+      const __amount = web3.utils.toWei(amount.toString(), "ether");
+      if (+__amount > +max) {
+        setErrorA("Amount cannot be greater than max");
+        setErrorAB(true);
+      } else if (+__amount < +min) {
+        setErrorA("Amount cannot be less than min");
+        setErrorAB(true);
+      } else {
+        setErrorA("");
+        setErrorAB(false);
+      }
+    }
+  },[web3, amount, min, max])
+
+  const handleChange = (e)=>{
+    setAmount(+e.target.value);
+  }
+
+  useEffect(() => {
+    const fetch = async () => {
+      if(!web3){
+        if (window.ethereum || window.web3) {
+          const web3 = new window.Web3(window.web3.currentProvider); 
+          setWeb3(web3);
+        }else{
+          setError("Please install MetaMask or TrustWallet");
+        }
+      }else{
+        let accounts = [];
+        try {
+          accounts = await web3.currentProvider.enable();
+          const _account = web3.utils.toChecksumAddress(accounts[0]);
+          if(_account !== account){
+            setAccount(_account);
+            setConnected(true);
+          }
+        } catch (e) {
+          setConnected(false);
+        }
+        if (id !== NETWORK_ID && id !== '') {
+          setLoading(false);
+          setError(
+            `Wrong network, Switch to Smart Chain ${NETWORK_ID === 97 && "Testnet"}`
+          );
+          return;
+        }
+        if (connected) {
+          const _mine = await contract.methods.getUserContribution(account).call();
+          const _isWhitelisted = await contract.methods.whitelist(account).call();
+
+          setMine(_mine);
+          setIsWhiteListed(_isWhitelisted);
+          
+          
+          setError(
+            `You've bought ${web3.utils.fromWei(
+              _mine.toString(),
+              "ether"
+            )} out of ${web3.utils.fromWei(max.toString(), "ether")} BNB`
+          );
+        }
+
+        if(loading){
+          setLoading(false);
+        }
+      }
+    }
+    fetch();
+  }, [account, $, web3, id, block]);
+
+  const connect = async (e) => {
+    try {
+      const accounts = await web3.currentProvider.enable();
+      setAccount(accounts[0]);
+      setConnected(true);
+    } catch (e) {
+      setConnected(false);
+    }
+  }
+  const buy = async (e) => {
+    e.preventDefault();
+    $(e.target).text("Buying...");
+    $(e.target).attr("disabled");
+    await contract.methods
+      .buyTokens(account)
+      .send({ from: account, value: web3.utils.toWei(amount.toString(), "ether") });
+    $(e.target).text("Buy Tokens");
+    $(e.target).removeAttr("disabled");
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {loading ? (
+          <p>Loading...</p>
+        ) : web3 ? (
+          connected ? (
+            error.startsWith("Wrong network") ? (
+              <p>{error}</p>
+            ) : started ? (
+              !completed ? (
+                <>
+                  <div style={{ fontSize: "16px" }}>
+                    Soft Cap: {web3.utils.fromWei(softcap.toString(), "ether")}{" "}
+                    BNB
+                    <br />
+                    Hard Cap: {web3.utils.fromWei(
+                      hardcap.toString(),
+                      "ether"
+                    )}{" "}
+                    BNB
+                    <br />
+                    Raised: {web3.utils.fromWei(raised.toString(), "ether")} BNB
+                    <br />
+                    Your Contribution:{" "}
+                    {web3.utils.fromWei(mine.toString(), "ether")} BNB
+                    <br />
+                    Min. Buy: {web3.utils.fromWei(min.toString(), "ether")} BNB
+                    <br />
+                    Max. Buy: {web3.utils.fromWei(max.toString(), "ether")} BNB
+                    <p>{onlyWhitelisted && isWhitelisted ? error : ""}</p>
+                    <p>{onlyWhitelisted && isWhitelisted ? errorA : ""}</p>
+                  </div>
+                  {onlyWhitelisted && isWhitelisted ? (
+                    <>
+                      <input
+                        type="number"
+                        placeholder="Enter amount"
+                        onChange={handleChange}
+                        style={{
+                          width: "40%",
+                          border: "none",
+                          padding: "14px 28px",
+                          fontSize: "16px",
+                          textAlign: "center",
+                        }}
+                      />
+                      <button
+                        onClick={buy}
+                        style={{
+                          marginTop: "10px",
+                          display:
+                            web3.utils.fromWei(mine.toString(), "ether") >=
+                              web3.utils.fromWei(max.toString(), "ether") ||
+                            errorAB
+                              ? "none"
+                              : "block",
+                          width: "50%",
+                          border: "none",
+                          backgroundColor: "#04AA6D",
+                          padding: "14px 28px",
+                          fontSize: "16px",
+                          cursor: "pointer",
+                          textAlign: "center",
+                        }}
+                      >
+                        Buy Tokens
+                      </button>
+                    </>
+                  ) : (
+                    <p>You are not whitelisted, wait for PUBLIC opening</p>
+                  )}
+                </>
+              ) : (
+                <p>Public sale has ended</p>
+              )
+            ) : (
+              <p>Public sale isn't live yet</p>
+            )
+          ) : (
+            <>
+              <p>{"Please login to MetaMask or TrustWallet"}</p>
+              <button
+                onClick={connect}
+                style={{
+                  marginTop: "10px",
+                  display: "block",
+                  border: "none",
+                  backgroundColor: "#04AA6D",
+                  padding: "14px 28px",
+                  fontSize: "16px",
+                  cursor: "pointer",
+                  textAlign: "center",
+                }}
+              >
+                Connect
+              </button>
+            </>
+          )
+        ) : (
+          <p>Install Metamask or TrustWallet</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,139 @@
+export const ADDRESS = '0xd6B689e7bC378454Eaeb53d44aD5AEd991b6Db34'
+export const ABI = [
+  {
+    inputs: [
+      { internalType: "address", name: "_beneficiary", type: "address" },
+    ],
+    name: "buyTokens",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cap",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "capReached",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "claimRefund",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "closingTime",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "contributions",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_beneficiary", type: "address" },
+    ],
+    name: "getUserContribution",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "goal",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "goalReached",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "hasClosed",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "isFinalized",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxContribution",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "minContribution",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "onlyWhitelisted",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "openingTime",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "rate",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "weiRaised",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "whitelist",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  }
+];
+export const NETWORK_ID = 97;
+
+// const t_address = "0xEBfeaDD37C5b0db20140Fe515740d0669529055E";
+// const t_address = "0xb96c7C6C233A57B3A0ee98cCB1500973B358e4CD";
+// const t_address = "0x927Acb7E34A112796C5c65590e20087c0d4fD94F";
+// const t_address = "0xd6B689e7bC378454Eaeb53d44aD5AEd991b6Db34";
